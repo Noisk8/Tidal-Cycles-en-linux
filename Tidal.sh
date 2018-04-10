90f908f4ba3db7e3d80e7b52b33961e4e0c73a9f
Noisk8 <juansk8jaramillo@gmail.com>
2018-04-09 22:55:45 -0400

    Update Tidal.sh

--- Tidal.sh
+++ Tidal.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 
-echo Installing dependencies..
+echo Instalando dependencias 
 sudo apt-get update
 sudo apt-get install haskell-platform supercollider sc3-plugins build-essential git qjackctl cabal-install zlib1g-dev libportmidi-dev libasound2-dev
 
@@ -11,16 +11,15 @@ sudo adduser $USER audio
 if [ -e /usr/bin/atom ]; then
     echo Atom Ya esta instalado
 else
-   echo Installing atom
+   echo Instalando atom
    wget --output-document=/tmp/atom.deb http://atom.io/download/deb
    sudo dpkg -i /tmp/atom.deb
    sudo apt --fix-broken install -y
 fi
    
-echo Installing/updating atom tidalcycles package
+echo Instalando actualizando paquete tidalcycles en atom
 apm install tidalcycles
 
-echo Installing/updating the tidal pattern engine, and tidal-midi
 cabal update
 cabal install Cabal
 cabal install tidal
@@ -30,17 +29,17 @@ cabal install tidal-midi
 mkdir -p ~/.local/share/SuperCollider/downloaded-quarks/
 cd ~/.local/share/SuperCollider/downloaded-quarks/
 if [ -d ./SuperDirt ]; then
-    echo ** Updating SuperDirt
+    echo ** Actualizando SuperDirt
     cd SuperDirt
     git pull
     cd -
 else
-    echo ** Installing SuperDirt quark
+    echo ** Instalando SuperDirt quark
     git clone https://github.com/musikinformatik/SuperDirt.git
 fi
 
 if [ -d ./Vowel ]; then
-    echo ** Updating Vowel quark
+    echo ** Actualizando Vowel quark
     cd Vowel
     git pull
     cd -
@@ -50,13 +49,11 @@ else
 fi
 
 if [ -d ./Dirt-Samples ]; then
-    echo ** Updating Dirt-Samples quark
+    echo ** Actualizando Dirt-Samples quark
     cd Dirt-Samples
     git pull
     cd -
 else
-    echo ** Installing Dirt-Samples quark, this may take a little while
+    echo ** Instalanso Dirt-Samples quark
     git clone https://github.com/tidalcycles/Dirt-Samples.git
 fi
-
-echo Install process complete. It is a good idea to reboot now.
